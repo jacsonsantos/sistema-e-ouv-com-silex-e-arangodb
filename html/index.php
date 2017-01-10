3c094ef4a7e402427643ce8f44efdc0c13db52d6
Jacson <jacsonk47@gmail.com>
2017-01-10 10:54:45 -0300

    finalizado o front do sistema e-ouv

---
+++ html/index.php
@@ -0,0 +1,19 @@
+<?php
+chdir(dirname(__DIR__));
+    require "vendor/autoload.php";
+
+    use Silex\Application;
+    use Symfony\Component\HttpFoundation\RedirectResponse;
+
+    $app = new Application;
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views',
+    ));
+    $app->register(new Silex\Provider\SerializerServiceProvider());
+    $app->register(new \JSantos\Provider\IndexServiceProvider());
+    $app->register(new \JSantos\Provider\RouteServiceProvider());
+    $app->register(new Silex\Provider\ServiceControllerServiceProvider());
+
+    $app->run();
